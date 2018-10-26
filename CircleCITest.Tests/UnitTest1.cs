6f53a00e18c39c5bb2e930201864362eb9411af6
NautilusJosh <josh.englis@mediware.com>
2018-10-26 10:19:50 -0500

    Addding unit tests project with failing test

---
+++ CircleCITest.Tests/UnitTest1.cs
@@ -0,0 +1,24 @@
+using System;
+using Xunit;
+
+namespace CircleCITest.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1_Success()
+        {
+            var result = false;
+
+            Assert.False(result, "False result");
+        }
+
+        [Fact]
+        public void Test2_Fail()
+        {
+            var result = true;
+
+            Assert.False(result, "True result, should fail");
+        }
+    }
+}
