3106e3afff1edee45ca036bc5183d8fb0af0222c
NautilusJosh <josh.englis@mediware.com>
2018-10-26 15:51:07 -0500

    Forcing unit tests to fail...as a test

--- CircleCITest.Tests/UnitTest1.cs
+++ CircleCITest.Tests/UnitTest1.cs
@@ -18,7 +18,7 @@ namespace CircleCITest.Tests
         {
             var result = true;
 
-            Assert.True(result, "True result, should fail");
+            Assert.False(result, "True result, should fail");
         }
     }
 }
